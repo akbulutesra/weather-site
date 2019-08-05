5917f3b7d88530744fad5dbe12b01ed675f234c1
akbulutesra <50598326+akbulutesra@users.noreply.github.com>
2019-08-05 21:03:39 +0200

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import './Footer.css';
 
 class Footer extends Component {
     render () {
         return (
-            <div>
-                <p>my footer</p>
+            <div className="FooterBox">
+                <div className="InfoBox">
+                    <p>Iletisim</p>
+                    <p>info@weather.com</p>
+                </div>
             </div>
         );
     }
